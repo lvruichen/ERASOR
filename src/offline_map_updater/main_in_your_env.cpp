f490f998cda5472414b78a029b7e5cff31f1a038
lvruichen <2374611812@qq.com>
2023-07-20 19:33:18 +0800

    feat: ✨fit for my dateset

--- src/offline_map_updater/main_in_your_env.cpp
+++ src/offline_map_updater/main_in_your_env.cpp
@@ -30,32 +30,50 @@ vector<float> split_line(string input, char delimiter) {
 void load_all_poses(string txt, vector<Eigen::Matrix4f >& poses){
     // These poses are already w.r.t body frame!
     // Thus, tf4x4 by pose * corresponding cloud -> map cloud
-    cout<<"Target path: "<< txt<<endl;
+    cout<<"Target path: "<< txt <<endl;
     poses.clear();
-    poses.reserve(2000);
+    poses.reserve(3000);
     std::ifstream in(txt);
     std::string line;
-
-    int count = 0;
-    while (std::getline(in, line)) {
-        if (count == 0){
-            count++;
-            continue;
+    while (getline(in, line)) {
+        float _data[12];
+        stringstream strstream(line);
+        string _out;
+        int i = 0;
+        while(strstream >> _out) {
+            float a = stof(_out);
+            _data[i] = a;
+            i++;
         }
-
-        vector<float> pose = split_line(line, ',');
-
-        Eigen::Translation3f ts(pose[2], pose[3], pose[4]);
-        Eigen::Quaternionf q(pose[8], pose[5], pose[6], pose[7]);
-        Eigen::Matrix4f tf4x4_cam = Eigen::Matrix4f::Identity(); // Crucial!
-        tf4x4_cam.block<3, 3>(0, 0) = q.toRotationMatrix();
-        tf4x4_cam.block<3, 1>(0, 3) = ts.vector();
-        Eigen::Matrix4f tf4x4_lidar = tf4x4_cam;
-        
-        poses.emplace_back(tf4x4_lidar);
-        count++;
+        Eigen::Isometry3f t = Eigen::Isometry3f::Identity();
+        t(0, 0) = _data[0], t(0, 1) = _data[1], t(0, 2) = _data[2], t(0, 3) = _data[3];
+        t(1, 0) = _data[4], t(1, 1) = _data[5], t(1, 2) = _data[6], t(1, 3) = _data[7];
+        t(2, 0) = _data[8], t(2, 1) = _data[9], t(2, 2) = _data[10], t(2, 3) = _data[11];
+        poses.push_back(t.matrix());
     }
-    std::cout<<"Total "<<count<<" poses are loaded"<<std::endl;
+    in.close();
+    std::cout<<"Total "<< poses.size() <<" poses are loaded"<<std::endl;
+}
+
+void readBinFile(pcl::PointCloud<pcl::PointXYZI>::Ptr cloudInptr, string& filePath){
+        cloudInptr->clear();
+        int32_t num = 10000000;
+        float *data = (float*)malloc(num * sizeof(float));
+        float *px = data + 0;
+        float *py = data + 1;
+        float *pz = data + 2;
+        float *pr = data + 3;//反射强度
+        fstream input(filePath.c_str(), ios::in | ios::binary);
+        if(!input.good()){
+        cerr << "Couldn't read in_file: " << filePath << endl;
+        }
+        for (int i = 0; input.good() && !input.eof(); i++) {
+            pcl::PointXYZI point;
+            input.read((char*) &point.x, 3*sizeof(float));
+            input.read((char*) &point.intensity, sizeof(float));
+            cloudInptr->push_back(point);
+        }
+        input.close();
 }
 
 int main(int argc, char **argv)
@@ -69,8 +87,6 @@ int main(int argc, char **argv)
     nh.param<int>("/init_idx", INIT_IDX, 0);
     nh.param<int>("/interval", INTERVAL, 2);
     nh.param<bool>("/stop_for_each_frame", STOP_FOR_EACH_FRAME, false);
-
-    std::string staticmap_path = std::getenv("HOME") + filename;
 
     // Set ROS visualization publishers
     ros::Publisher NodePublisher = nh.advertise<erasor::node>("/node/combined/optimized", 100);
@@ -89,9 +105,8 @@ int main(int argc, char **argv)
      */
     // Set target data
     cout << "\033[1;32mTarget directory:" << DATA_DIR << "\033[0m" << endl;
-    string raw_map_path = DATA_DIR + "/dense_global_map.pcd";
-    string pose_path = DATA_DIR + "/poses_lidar2body.csv";
-    string pcd_dir = DATA_DIR + "/pcds"; //
+    string pose_path = DATA_DIR + "/pose.txt";
+    string pcd_dir = DATA_DIR + "/velodyne"; //
     // Load raw pointcloud
 
     vector<Eigen::Matrix4f> poses;
@@ -103,9 +118,8 @@ int main(int argc, char **argv)
         signal(SIGINT, erasor_utils::signal_callback_handler);
 
         pcl::PointCloud<PointType>::Ptr srcCloud(new pcl::PointCloud<PointType>);
-        string pcd_name = (boost::format("%s/%06d.pcd") % pcd_dir % i).str();
-        erasor_utils::load_pcd(pcd_name, srcCloud);
-
+        string pcd_name = (boost::format("%s/%06d.bin") % pcd_dir % i).str();
+        readBinFile(srcCloud, pcd_name);
         erasor::node node;
         node.header.seq = i;
         node.odom = erasor_utils::eigen2geoPose(poses[i]);
